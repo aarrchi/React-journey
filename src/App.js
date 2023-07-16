bcc592b2a788279fcf70104528714d0f08f63d56
aarrchi <archielama957@gmail.com>
2023-07-16 17:12:33 +0545

    feat: array in component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {Button} from './Button';
 
 function AppComponent(){
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <div>
-       <h1>hello</h1>
-      <h2>Hi</h2>
+      {
+        array.map((arr) => {
+           return <div key={arr}>Hello world</div>
+        })
+      }
+      
       <Button></Button>
     </div>
    
